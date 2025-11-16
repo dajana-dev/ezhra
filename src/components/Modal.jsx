92b54510cf892559aba276dff5746ec0f585be61
Dajana-Dev <dajanapajkic.dev@gmail.com>
2025-11-16 17:20:58 +0100

    add basic Modal css

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,11 @@
+import "../styles/Modal.scss"
+
 const Modal = ({isModalOpen, handleCancelDelete, handleDelete, message}) => {
     if(!isModalOpen) return null;
 
     return (
         <div className="modal">
-            <h3>{message}</h3>
+            <h4>{message}</h4>
             <div className="modal-buttons">
                 <button className="delete" onClick={handleDelete}>Delete</button>
                 <button className="cancel" onClick={handleCancelDelete}>Cancel</button>
